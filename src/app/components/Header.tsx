dbf89662fdba178d7cc48ed70ecf63b5ea361e2f
satou-yosiyuki <stuysyk1027@icloud.com>
2024-10-20 21:27:50 +0900

    権限によってHeaderの表示を変更

---
+++ src/app/components/Header.tsx
@@ -0,0 +1,49 @@
+import React from "react";
+import { isPropsOfType } from "../type/utils";
+
+type Tag = "admin" | "user";
+
+type BaseHeaderProps = {
+  tag: Tag;
+  title: string;
+  onPress: () => void;
+};
+
+type AdminHeaderProps = BaseHeaderProps & {
+  tag: "admin";
+  leftButton: React.ReactNode;
+};
+type UserHeaderProps = BaseHeaderProps & {
+  tag: "user";
+  rightButton: React.ReactNode;
+};
+
+type HeaderProps = AdminHeaderProps | UserHeaderProps;
+
+function Header(props: HeaderProps) {
+  const { title, onPress } = props;
+  const isAdmin = isPropsOfType<HeaderProps, AdminHeaderProps>(props, "admin");
+  const isUser = isPropsOfType<HeaderProps, UserHeaderProps>(props, "user");
+
+  return (
+    <header className="bg-blue-500 text-white">
+      <div className="container mx-auto px-4 py-4 flex justify-between items-center">
+        {isAdmin && props.leftButton}
+
+        <div className="flex-1 text-center">
+          <div className="text-2xl font-bold">{title}</div>
+          <button
+            className="bg-white text-blue-500 px-4 py-2 mt-4 rounded hover:bg-gray-100"
+            onClick={() => onPress()}
+          >
+            onPress
+          </button>
+        </div>
+
+        {isUser && props.rightButton}
+      </div>
+    </header>
+  );
+}
+
+export default Header;
